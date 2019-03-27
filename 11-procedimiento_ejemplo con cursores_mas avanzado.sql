c25379400f7fe05fdec2e431c0e17e9097618e8d
Pepe <pepelluyot@gmail.com>
2019-03-27 12:35:34 +0100

    implementando procedimiento para ver si una tabla existe

--- 11-procedimiento_ejemplo con cursores_mas avanzado.sql
+++ 11-procedimiento_ejemplo con cursores_mas avanzado.sql
@@ -1,10 +1,22 @@
 use BD06_1819;
+show tables like 'datos%';
+
+select count(*) from information_schema.tables where table_name="datos_personales"
+and table_Schema='BD06_1819';
+
+select exists(select table_name from information_schema.tables where table_name="datos_personales"
+and table_Schema='BD06_1819');
+
+set @existe = exists(select table_name from information_schema.tables where table_name="datos_personales"
+and table_Schema='BD06_1819');
+
+
 drop procedure if exists datospersonales;
 delimiter $$
 create procedure datospersonales()
-begin
+proc: begin
 	declare v_fecha DATE;
-    declare v_fin boolean default 0;
+    declare v_fin, v_existe_tabla boolean default 0;
     declare v_usuario varchar(10);
 	declare v_cursor cursor for 
         select usuario  from usuarios;
@@ -12,14 +24,22 @@ begin
 	declare continue handler for not found
 		set v_fin=1;
         
-drop table if exists datos_personales;
-create table datos_personales (
-	usuario varchar(10) NOT NULL PRIMARY KEY,
-    fecha_nacimiento date NULL,
-    ultimo_login timestamp NULL,
-    num_accesos INT NULL default 0,
-    foreign key (usuario) references  usuarios (usuario) ON DELETE CASCADE
-);
+	-- drop table if exists datos_personales;
+	-- comprobamos si existe la tabla:
+	set v_existe_tabla = exists(select table_name from information_schema.tables where table_name="datos_personales"
+	and table_Schema='BD06_1819');
+	if v_existe_tabla then
+		select "salimos del procedimiento";
+		leave proc;
+	end if;
+		
+	create table datos_personales (
+			usuario varchar(10) NOT NULL PRIMARY KEY,
+			fecha_nacimiento date NULL,
+			ultimo_login timestamp NULL,
+			num_accesos INT NULL default 0,
+			foreign key (usuario) references  usuarios (usuario) ON DELETE CASCADE
+		);
 	
     open v_cursor;
 		mibucle: LOOP
@@ -36,19 +56,13 @@ create table datos_personales (
         END LOOP mibucle;
     close v_cursor;
 
-end $$
+end proc $$
 delimiter ;
 call datospersonales();
 select rand();
 
 select * from datos_personales;
 
--- vamos a cambiar algunas fechas de nacimiento a mano para hacer la prueba con el procedimiento que vamos a crear
-UPDATE `datos_personales` SET `fecha_nacimiento`='2001-06-08' WHERE `usuario`='juan';
-UPDATE `datos_personales` SET `fecha_nacimiento`='1984-10-25' WHERE `usuario`='manuel';
-UPDATE `datos_personales` SET `fecha_nacimiento`='1985-05-15' WHERE `usuario`='lolo';
-
-select * from datos_personales;
 
 drop procedure if exists historico_sp;
 delimiter $$
@@ -58,29 +72,29 @@ begin
     declare v_fecNac date default NULL;
     
     case (select count(*) from usuarios where usuario = _user)
-    when 0 then
-		select 'usuario no existe';
-	else
-		select 'usuario existe';
-        -- si el usuario existe comprobamos su fecha de nacimiento
-		select fecha_nacimiento into v_fecNac from datos_personales where usuario = _user;
-        set v_edad = TIMESTAMPDIFF(YEAR,v_fecNac ,current_date());
-        case
-		when v_edad is NULL then
-        	select 'el usuario no tiene una edad determinada';
-        when v_edad >= 18 then
-			select 'mayor de edad';
-		when v_edad between 1 and 17 then
-			select 'menor de edad';
-		end case;	
-	end case;
+		when 0 then
+			select 'usuario no existe';
+		else
+			select 'usuario existe';
+			-- si el usuario existe comprobamos su fecha de nacimiento
+			select fecha_nacimiento into v_fecNac from datos_personales where usuario = _user;
+			set v_edad = TIMESTAMPDIFF(YEAR,v_fecNac ,current_date());
+			case
+				when v_edad is NULL then
+					select 'el usuario no tiene una edad determinada';
+				when v_edad >= 18 then
+					select 'mayor de edad';
+				when v_edad between 1 and 17 then
+					select 'menor de edad';
+			end case;	
+		end case;
     
 end $$
 delimiter ;
 
-call ejemplo11_sp('pepe');
-call ejemplo11_sp('juan');
-call ejemplo11_sp('rosa');
+call historico_sp('pepe');
+call historico_sp('juan');
+call historico_sp('rosa');
 
 /*
 Crea un procedimiento (​ ejemplo11_sp.sql​ ) 
